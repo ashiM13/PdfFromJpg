15162e365498f86d5a2a5e87d7a82ce11a4de7e5
Миша Копта <misha13kopta@gmail.com>
2020-09-21 20:16:16 +0200

    Добавьте файлы проекта.

---
+++ PdfFromJpg.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import os
+
+def get_name(source):
+    head = source.split('.')[0]
+    return head + '.pdf'
+
+def pdf_make(dir):
+    imagelist = []
+    files = [f for f in os.listdir('.') if f.endswith('.jpg')]
+    for i in range(len(files)):
+        image = Image.open(files[i])
+        imagelist.append(image)
+        fname = get_name(files[0])
+        image = Image.open(files[0])
+    image.save(fname, save_all=True, append_images=imagelist[1:])
+
+if __name__ == '__main__':
+    pdf_make('.')
