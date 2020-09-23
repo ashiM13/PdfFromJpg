b2c45bd54d462b78d68ccf4716a3be6e87ce3383
Миша Копта <misha13kopta@gmail.com>
2020-09-23 18:50:14 +0200

    Replaced list with directory

--- PdfFromJpg.py
+++ PdfFromJpg.py
@@ -1,19 +1,18 @@
-from PIL import Image
 import os
+from PIL import Image
 
 def get_name(source):
     head = source.split('.')[0]
     return head + '.pdf'
 
-def pdf_make(dir):
-    imagelist = []
-    files = [f for f in os.listdir(dir) if f.endswith('.jpg')]
+def pdf_make(directory):
+    d = {}
+    files = [f for f in os.listdir(directory) if f.endswith('.jpg')]
     for i in range(len(files)):
-        image = Image.open(files[i])
-        imagelist.append(image)
+        d[i] = Image.open(files[i])
         fname = get_name(files[0])
         image = Image.open(files[0])
-    image.save(fname, save_all=True, append_images=imagelist[1:])
+    image.save(fname, save_all=True, append_images=d.values())
 
 if __name__ == '__main__':
     pdf_make('.')
