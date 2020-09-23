6c603314ba5bef233972d95a599cf26e6838409e
Миша Копта <misha13kopta@gmail.com>
2020-09-23 19:08:19 +0200

    Mic. correction

--- PdfFromJpg.py
+++ PdfFromJpg.py
@@ -8,10 +8,10 @@ def get_name(source):
 def pdf_make(directory):
     d = {}
     files = [f for f in os.listdir(directory) if f.endswith('.jpg')]
-    for i in range(len(files)):
+    for i in range(1, len(files)):
         d[i] = Image.open(files[i])
-        fname = get_name(files[0])
         image = Image.open(files[0])
+        fname = get_name(files[0])
     image.save(fname, save_all=True, append_images=d.values())
 
 if __name__ == '__main__':
